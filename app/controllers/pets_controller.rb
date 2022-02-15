ad4618ea01ced9dde08c951f1daf4649fb3c2d2d
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-15 18:56:01 +0000

    NOTE: Add notese for private methods

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,10 +13,12 @@ class PetsController < ApplicationController
   # We've abstracted away all this code with our before action above, the method is a private method only accessible by our controller
   def show; end
 
+  # We always need to pass a new instance of our model to the form, think of it as passing a blank canvas for the user to then provide attribute information for through the form
   def new
     @pet = Pet.new
   end
 
+  # What is this pet_params?  See the private method below 👇
   def create
     pet = Pet.new(pet_params)
     pet.save!
@@ -38,11 +40,12 @@ class PetsController < ApplicationController
   end
 
   private
-
+  # Our little before action method that sets which pet we want to interact with before certain controller actions
   def set_pet
     @pet = Pet.find(params[:id])
   end
 
+  # This is the principle of using Strong Parameters.  The core idea here is we want to make sure we only allow specific information from the user to come through, specifically information we asked for, nothing more.  This prevents nefarious users from providing info that we may not want.  To get a better understanding of this, throw a raise into the create action and see the difference between the regular params and pokemon params in the little console at the bottom of the page, 👀 peep the permitted value
   def pet_params
     params.require(:pet).permit(:name, :address, :age, :found_on, :species)
   end
