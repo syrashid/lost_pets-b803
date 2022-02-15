b67929ffa11a6a12e4676ecd8c8a146721e52fd9
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-15 18:44:46 +0000

    FEAT: Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing the Lost Pet Database"
+Pet.destroy_all
+
+puts "Generating Pokemon"
+50.times do
+  temp_pet = Pet.create( name: Faker::Creature::Dog.name,
+                  address: Faker::Address.street_address,
+                  species: PET::SPECIES.sample,
+                  found_on: Date.today - rand(1..21)),
+                  age: rand(1..10)
+  puts "Generated #{temp_pet.name}"
+end
+
+puts "Generated #{Pet.count} pets"
