e84421fee0b287cef04a4920e8f2f7e37d56e7ba
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-15 18:22:41 +0000

    FEAT: Finish livecode

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,14 +1,43 @@
 class PetsController < ApplicationController
-  before_action :set_pet, except: [:index]
+  before_action :set_pet, except: [:index, :new, :create]
+
   def index
     @pets = Pet.all
   end
 
   def show; end
 
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+    pet.save!
+    redirect_to pet_path(pet)
+  end
+
+  def edit; end
+
+  def update
+    @pet.update(pet_params)
+
+    redirect_to pet_path(@pet)
+  end
+
+  def destroy
+    @pet.destroy
+
+    redirect_to pets_path
+  end
+
   private
 
   def set_pet
     @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :address, :age, :found_on, :species)
   end
 end
