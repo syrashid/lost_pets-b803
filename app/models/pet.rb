6b443723a102da0b8f3baa7b176e71be1f55ad30
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-15 18:33:32 +0000

    FEAT: Update pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,9 +1,10 @@
 class Pet < ApplicationRecord
+  SPECIES = %w(dog cat rabbit snake turtle horse donkey)
   validates :name, presence: true
   validates :found_on, presence: true
-  validates :species, inclusion: { in: %w(dog cat rabbit snake turtle horse donkey)}
+  validates :species, inclusion: { in: SPECIES }
 
   def found_days_ago
-    "#{(Date.today - found_on).to_i} day(s) ago"
+    "#{(Date.today - found_on).to_i}"
   end
 end
