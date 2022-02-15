dbdc970e21d00fb25701409c7bdcd701a3441c01
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-15 18:51:27 +0000

    REFACTOR: Use resources in places of hardcoded routees

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  get "pets", to: "pets#index"
-  get "pets/new", to: "pets#new", as: :new_pet
-  post "pets", to: "pets#create"
-  get "pets/:id", to: "pets#show", as: :pet
-  get "pets/:id/edit", to: "pets#edit", as: :edit_pet
-  patch "pets/:id", to: "pets#update"
-  delete "pets/:id", to: "pets#destroy"
+
+  resources :pets
+  # 👆 This single line, replaces all of this 👇
+
+  # # A user can see all pets
+  # get "pets", to: "pets#index"
+  # # A user can report a lost pet
+  # get "pets/new", to: "pets#new", as: :new_pet
+  # post "pets", to: "pets#create"
+  # # A user can see info on a single pet
+  # get "pets/:id", to: "pets#show", as: :pet
+  # # A user can edit details of a pet
+  # get "pets/:id/edit", to: "pets#edit", as: :edit_pet
+  # patch "pets/:id", to: "pets#update"
+  # # A user can remove a pet from the site
+  # delete "pets/:id", to: "pets#destroy"
+
+
 end
