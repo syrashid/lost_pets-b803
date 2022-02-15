5875f7badf7a8af742b893ae953182370c4689bd
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-15 17:44:13 +0000

    FEAT: Add pet show page

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,9 @@
 class Pet < ApplicationRecord
   validates :name, presence: true
+  validates :found_on, presence: true
   validates :species, inclusion: { in: %w(dog cat rabbit snake turtle horse donkey)}
+
+  def found_days_ago
+    "#{(Date.today - found_on).to_i} day(s) ago"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "pets", to: "pets#index"
+  get "pets/:id", to: "pets#show", as: :pet
 end
